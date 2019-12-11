ebca0f27949ccd3d2dc3bb72d11e1868028dbeff
下午茶溢栈 <zpm683@qq.com>
2019-12-11 12:24:23 +0900

    Update react-app-env.d.ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+declare module "*.svg";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.bmp";
+declare module "*.tiff";
