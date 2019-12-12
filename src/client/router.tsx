1be0e3ff4706bc337586152efda3b0a00b1db362
下午茶溢栈 <zpm683@qq.com>
2019-12-12 09:57:41 +0900

    Update router.tsx

--- src/client/router.tsx
+++ src/client/router.tsx
@@ -1,1 +1,33 @@
+import React from "react";
+import { Router, Switch, Route, Redirect } from "react-router";
+import { createBrowserHistory } from "history";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "./theme";
+import { routerConfig, BASE_URL } from "./common/config/routerList";
+const history = createBrowserHistory();
 
+export default () => {
+  return (
+    <div>
+      <CssBaseline />
+      <MuiThemeProvider theme={theme}>
+        <Router history={history}>
+          <Switch>
+            { routerConfig.map((oneRooter, index) => {
+              return (
+                <Route
+                  key={index}
+                  exact={oneRooter.exact}
+                  path={oneRooter.path}
+                  component={oneRooter.component}
+                />
+              );
+            })}
+            <Redirect exact to={`${BASE_URL}404NotFound`} />
+          </Switch>
+        </Router>
+      </MuiThemeProvider>
+    </div>
+  );
+};
