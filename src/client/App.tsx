2879ee061b9b457f54393b8244a7bd2ac443b793
下午茶溢栈 <zpm683@qq.com>
2019-12-12 09:56:54 +0900

    Update App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * APP
+ * @author FXS)zhang.puming
+ */
 
-const App: React.FC = () => {
+import React from "react";
+import WebRouter from "./router";
+import { Provider } from "react-redux";
+import store, { persistor } from "./store";
+import "./common/i18n/init";
+
+import { PersistGate } from "redux-persist/integration/react";
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <WebRouter />
+      </PersistGate>
+    </Provider>
   );
-}
-
-export default App;
+};
