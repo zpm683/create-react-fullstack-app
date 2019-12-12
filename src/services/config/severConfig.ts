7ef9edc4fb40ae74ae69969d4e24f6d3e5e1dca9
下午茶溢栈 <zpm683@qq.com>
2019-12-12 09:44:27 +0900

    Create severConfig.ts

---
+++ src/services/config/severConfig.ts
@@ -0,0 +1,10 @@
+interface IServerConfig {
+    apiUrl:string;
+    ip:string;
+    prot:string;
+}
+export const serverConfig: IServerConfig = {
+    apiUrl:"xxxxxxx",
+    ip:"0.0.0.0",
+    prot:"9999"
+}
