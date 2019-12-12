ea7cda247c9614c31d5ec0d87698ec8430840030
下午茶溢栈 <zpm683@qq.com>
2019-12-12 09:54:16 +0900

    Create keys.ts

---
+++ src/client/common/i18n/keys.ts
@@ -0,0 +1,16 @@
+/**
+ * keywords file
+ */
+
+/**
+ * ILocalesKeys is the interface about keywords 
+ */
+export interface ILocalesKeys {
+    //TODO: add other keywords
+    readonly HELLO: string;
+}
+
+export const TEXT: ILocalesKeys = {
+    HELLO:"HELLO",
+};
+  
