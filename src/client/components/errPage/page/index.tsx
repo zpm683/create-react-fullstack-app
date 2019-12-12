648cc3ba0a8008b24fa4489c46fafeebc2862fd9
下午茶溢栈 <zpm683@qq.com>
2019-12-12 09:55:41 +0900

    Create index.tsx

---
+++ src/client/components/errPage/page/index.tsx
@@ -0,0 +1,8 @@
+// TODO: import & export other ErrPage Template
+import NotFound404 from "../template/404";
+// import ServerErr505 from "../template/505";
+
+export {
+  NotFound404,
+  //ServerErr505
+};
