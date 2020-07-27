af966ab6dfb5f6d21dc3e68b3e14938796a5b885
下午茶溢栈 <zpm683@qq.com>
2020-07-27 17:55:03 +0900

    Update react-app-env.d.ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,18 +1,1 @@
 /// <reference types="react-scripts" />
-declare module "*.svg";
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.gif";
-declare module "*.bmp";
-declare module "*.tiff";
-
-declare module "wangeditor"
-
-declare module "react-swipeable-views-utils" {
-  const content: {
-    autoPlay: Function;
-  };
-  export = content;
-}
-
