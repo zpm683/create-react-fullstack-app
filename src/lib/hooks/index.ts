4a3d5f0152ee2f906eb7bdaec801fa7dd973a49a
下午茶溢栈 <zpm683@qq.com>
2020-08-13 21:07:35 +0900

    add useOnline Hook

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -2,3 +2,23 @@
  * 自定义 hooks
  */
 
+/**
+ * check is online or offline
+ * @returns true:Online false:Offline
+ * @example const isOnline = useOnline();
+ */
+export function useOnline() {
+  const [isOnline, setNetwork] = useState(window.navigator.onLine);
+  const updateNetwork = () => {
+    setNetwork(window.navigator.onLine);
+  };
+  useEffect(() => {
+    window.addEventListener("offline", updateNetwork);
+    window.addEventListener("online", updateNetwork);
+    return () => {
+      window.removeEventListener("offline", updateNetwork);
+      window.removeEventListener("online", updateNetwork);
+    };
+  });
+  return isOnline;
+}
