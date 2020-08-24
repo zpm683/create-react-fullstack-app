9784e403ec6a4756b6a6914b12006163a5a52b9f
下午茶溢栈 <zpm683@qq.com>
2020-08-24 15:40:19 +0900

    Create loadingSlice.ts

---
+++ src/app/common/redux/loadingSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../../store";
+import { LoadingState } from "common.params.loading";
+
+const initialState: LoadingState = {
+  isLoading: false,
+  errMsg: "",
+};
+
+const loadingSlice = createSlice({
+  name: "loading",
+  initialState,
+  reducers: {
+    /** setIsLoading */
+    setIsLoading(state, action: PayloadAction<boolean>) {
+      state.isLoading = action.payload;
+    },
+    /** setErrMsg */
+    setErrMsg(state, action: PayloadAction<string>) {
+      state.isLoading = false;
+      state.errMsg = action.payload;
+    },
+    /** cleanErrMsg */
+    cleanErrMsg(state, action: PayloadAction<void>) {
+      state.isLoading = false;
+      state.errMsg = "";
+    },
+  },
+});
+
+export const selectLoading = (state: RootState) => state.common.loading;
+export const { setErrMsg, cleanErrMsg, setIsLoading } = loadingSlice.actions;
+export default loadingSlice.reducer;
