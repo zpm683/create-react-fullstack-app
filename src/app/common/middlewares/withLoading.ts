3274b5e0618f3f7ed12649598d82a52e213549ce
下午茶溢栈 <zpm683@qq.com>
2020-08-24 15:37:51 +0900

    Create withLoading.ts

---
+++ src/app/common/middlewares/withLoading.ts
@@ -0,0 +1,52 @@
+import { AppThunk } from "../../../store";
+import { cleanErrMsg, setIsLoading, setErrMsg } from "../redux";
+type AppThunkDispatch = Parameters<AppThunk>[0];
+type AppThunkStore = Parameters<AppThunk>[1];
+
+/**
+ * withLoading
+ * @param asyncCommand
+ * @param onSuccess
+ * @param onFailure
+ * @example
+ * export const doLoginApi = (params: RequestData): AppThunk =>
+  withLoading<ResponseData>(
+    async (dispatch, getState) => {
+      return await callLoginApi(params);
+    },
+    (result, dispatch, getState) => {
+      console.log(`result${result}`);
+      dispatch(loginActions.setToken("test Token"));
+    },
+    (dispatch, getState) => {
+      console.log(getState().common.loading.errMsg);
+    }
+  );
+ */
+export function withLoading<T>(
+  asyncCommand: (dispatch: AppThunkDispatch, getStates: AppThunkStore) => Promise<T>,
+  onSuccess: (result: T, dispatch: AppThunkDispatch, getStates: AppThunkStore) => void = () => {},
+  onFailure: (dispatch: AppThunkDispatch, getStates: AppThunkStore) => void = () => {}
+): AppThunk {
+  return async (dispatch, getState) => {
+    try {
+      dispatch(cleanErrMsg());
+      dispatch(setIsLoading(true));
+      const result = await asyncCommand(dispatch, getState);
+      if (result) {
+        setTimeout(() => {
+          onSuccess(result, dispatch, getState);
+          dispatch(setIsLoading(false));
+        }, 1000);
+      } else {
+        setTimeout(() => {
+          onFailure(dispatch, getState);
+          dispatch(setErrMsg("result Err"));
+        }, 1000);
+      }
+    } catch (error) {
+      onFailure(dispatch, getState);
+      dispatch(setErrMsg(JSON.stringify(error)));
+    }
+  };
+}
