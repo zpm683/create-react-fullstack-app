bc7cd509f1816e119520fc07c5d934176bd76320
下午茶溢栈 <zpm683@qq.com>
2020-11-02 08:58:06 +0900

    Update ProcessEnvUtils.ts

--- src/app/common/utils/ProcessEnvUtils.ts
+++ src/app/common/utils/ProcessEnvUtils.ts
@@ -1,69 +1,29 @@
-/**
- * ProcessEnvUtils
- */
-export class ProcessEnvUtils {
-  /**
-   * production モードの場合、true
-   * ※ $ npm run build || yarn build 実行時に true となります。
-   */
-  static isProduction() {
-    return process.env.NODE_ENV === "production";
-  }
+import { fomartString, fomartStringToNumber } from "./dataUtils";
 
-  /**
-   * development モードの場合、true
-   * ※ $ npm start || yarn start 実行時に true となります。
-   */
-  static isDevelopment() {
-    return process.env.NODE_ENV === "development";
-  }
+export const isProduction = () => {
+  return process.env.NODE_ENV === "production";
+};
 
-  /**
-   * test モードの場合、true
-   * ※ $ npm run test || yarn test 実行時に true となります。
-   */
-  static isTest() {
-    return process.env.NODE_ENV === "test";
-  }
+export const isDevelopment = () => {
+  return process.env.NODE_ENV === "development";
+};
 
-  /**
-   * baseURLを取得します。
-   */
-  static getBaseURL() {
-    return _getString(process.env.REACT_APP_BASE_URL);
-  }
+export const isTest = () => {
+  return process.env.NODE_ENV === "test";
+};
 
-  /**
-   * ApiServerURLを取得します。
-   */
-  static getLogServerURL() {
-    return _getString(process.env.REACT_APP_LOG_URL);
-  }
+export const getBaseURL = () => {
+  return fomartString(process.env.REACT_APP_BASE_URL);
+};
 
-  /**
-   * ApiServerURLを取得します。
-   */
-  static getApiServerURL() {
-    return _getString(process.env.REACT_APP_API_URL);
-  }
+export const getLogServerURL = () => {
+  return fomartString(process.env.REACT_APP_LOG_URL);
+};
 
-  /**
-   * Web APIのタイムアウト値（ミリ秒）を取得します。
-   */
-  static getApiTimeout() {
-    return _getNumber(process.env.REACT_APP_API_TIMEOUT);
-  }
-}
+export const getApiServerURL = () => {
+  return fomartString(process.env.REACT_APP_API_URL);
+};
 
-function _getString(env: string | undefined): string {
-  return env ? env : "";
-}
-
-//eslint-disable-next-line
-function _getBoolean(env: string | undefined): boolean {
-  return !!env && env.toLowerCase() === "true";
-}
-
-function _getNumber(env: string | undefined): number {
-  return env ? Number(env) : 0;
-}
+export const getApiTimeout = () => {
+  return fomartStringToNumber(process.env.REACT_APP_API_TIMEOUT);
+};
