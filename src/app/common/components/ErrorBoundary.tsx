68e48dee9c3b1e0f45c26e4037595baac9247c22
下午茶溢栈 <zpm683@qq.com>
2020-11-02 08:46:10 +0900

    Create ErrorBoundary.tsx

---
+++ src/app/common/components/ErrorBoundary.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+type Props = {
+  renderError?: (error: Error) => React.ReactNode;
+  onError?: (error: Error, errorInfo: React.ErrorInfo) => void;
+  children?: React.ReactNode;
+};
+
+type State =
+  | {
+      hasError: false;
+    }
+  | {
+      hasError: true;
+      error: Error;
+    };
+
+export class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Readonly<Props>) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static defaultProps: Partial<Props> = {
+    onError: (error, errorInfo) => {
+      console.log(
+        `%cERROR:${error}`,
+        "color: red",
+      );
+    },
+    renderError: (error) => {
+      return defaultFallbackComponent(error);
+    },
+  };
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    if (this.props.onError) {
+      this.props.onError(error, errorInfo);
+    }
+  }
+
+  render() {
+    if (this.props.renderError) {
+      if (this.state.hasError) {
+        return this.props.renderError(this.state.error);
+      }
+    }
+    return this.props.children;
+  }
+}
+
+function defaultFallbackComponent(error: Error) {
+  return (
+    <div>
+      HAVE ERROR!
+    </div>
+  );
+}
