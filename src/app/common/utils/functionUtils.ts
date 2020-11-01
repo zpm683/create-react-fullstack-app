e19259ee0d94e8d256862689b4fb9ec93089fb1c
下午茶溢栈 <zpm683@qq.com>
2020-11-02 08:56:52 +0900

    Update functionUtils.ts

--- src/app/common/utils/functionUtils.ts
+++ src/app/common/utils/functionUtils.ts
@@ -7,3 +7,9 @@
  */
 export const wait = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+/**
+ * get a UUID
+ */
+export const getUUID = () =>
+  `${Math.random().toString(36).substring(2)}${Date.now().toString(36)}`;
