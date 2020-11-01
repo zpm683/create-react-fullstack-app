aa3ea22a67eace7f55f39f0d479ffd7b1820af76
下午茶溢栈 <zpm683@qq.com>
2020-11-02 08:54:52 +0900

    Update and rename DataUtils.ts to dataUtils.ts

--- src/app/common/utils/DataUtils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * DataUtils
- */
-export class DataUtils {
-  static isNullObject(obj: Object) {
-    return Object.keys(obj).length === 0 ? true : false;
-  }
-}
---
+++ src/app/common/utils/dataUtils.ts
@@ -0,0 +1,27 @@
+/**
+ * 判断对象内是否为空
+ */
+export const isEmptyObject = (obj: Object) => {
+  return Object.keys(obj).length === 0 ? true : false;
+};
+
+export const isEmptyObject = (obj: Object) => {
+  return Object.keys(obj).length === 0 ? true : false;
+};
+
+/**
+ * 一元数组转换二元数组
+
+ * @param arr
+ * @param row
+ * @return dyadicArray
+ * @example convertDyadicArray([2,3,4,5,6,7], 3) => [[2,3],[4,5],[6,7]]
+ */
+export const convertDyadicArray = (arr: string[], row: number) => {
+  let dyadicArray = [];
+  const col = arr.length / row;
+  for (let i = 0; i < row; i++) {
+    dyadicArray.push(arr.slice(i * col, (i + 1) * col));
+  }
+  return dyadicArray;
+};
