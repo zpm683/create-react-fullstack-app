8f2e7b6a851fd6e66f40cffca9e77d3df4715f3a
下午茶溢栈 <zpm683@qq.com>
2020-11-03 09:18:54 +0900

    Update main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ["../src/stories/**/*.stories.@(tsx|mdx)"],
-  addons: ["@storybook/addon-essentials"],
+  addons: ["@storybook/addon-essentials", "@storybook/preset-create-react-app"],
 };
