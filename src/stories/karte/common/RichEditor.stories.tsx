08e6a6b1e602ef2189bbb4be7a842444722190ce
下午茶溢栈 <zpm683@qq.com>
2020-11-05 09:26:41 +0800

    Add files via upload

---
+++ src/stories/karte/common/RichEditor.stories.tsx
@@ -0,0 +1,14 @@
+/**
+ * StoryBook: karte/components/RichEditor
+ * @author FXS)zhang.puming
+ */
+
+import React from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import { Typography } from "@material-ui/core";
+
+export default {
+  title: "karte/components/RichEditor",
+} as Meta;
+
+export const Demo = () => <Typography>TODO: add RichEditor</Typography>;
