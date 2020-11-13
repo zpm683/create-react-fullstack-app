e6526264f1cc9ecb4d9ff8501074f7550670cf54
下午茶溢栈 <zpm683@qq.com>
2020-11-13 15:51:04 +0900

    Create preview.js

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+export const parameters = {
+  options: {
+    storySort: {
+      method: "alphabetical",
+      order: [
+        "React全栈指北",
+        ["前言", "目录", "作者致谢"],
+        "React",
+        "Electron",
+        "React-Native",
+      ],
+    },
+  },
+};
