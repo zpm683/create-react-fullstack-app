9b2357894e2d2ce78e0dfc559dc30d6ae77872b7
下午茶溢栈 <zpm683@qq.com>
2020-12-08 09:47:15 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { App } from "./app";
 import * as serviceWorker from "./serviceWorker";
 import "./app/@common/i18n/init";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
